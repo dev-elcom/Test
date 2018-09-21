b58448a6485b204570f11ba6b9eb3e0508ab0f2b
dev-elcom <dev@elcomspb.ru>
2018-09-21 09:11:10 +0300

    first commit

---
+++ js/base.js
@@ -0,0 +1,36 @@
+function getQuestions(){
+	return [
+		{
+			number: 1,
+			question: 'Текст вопроса',
+			options: [
+				{
+					text: 'Вариант 1'
+				},
+				{
+					text: 'Вариант 2'
+				},
+				{
+					text: 'Вариант 3'
+				}
+			],
+			right: 1
+		},
+		{
+			number: 2,
+			question: 'Текст вопроса 2',
+			options: [
+				{
+					text: 'Вариант 12'
+				},
+				{
+					text: 'Вариант 22'
+				},
+				{
+					text: 'Вариант 32'
+				}
+			],
+			right: 3
+		}
+	];
+}
